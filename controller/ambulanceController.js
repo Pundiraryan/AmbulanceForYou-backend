87defa77d71e864bd3f3e26c13d50e4ac49af5c1
Aryan <aryanpundir0606@gmail.com>
2024-01-08 15:51:00 +0530

    controllers created

--- controller/ambulanceController.js
+++ controller/ambulanceController.js
@@ -20,3 +20,45 @@ const createAmbulance=async (req,res) => {
         console.log('save failed');
     }
 }
+
+const findAmbulance = async (req,res) => {
+    const {ambulanceid} = req.params;
+    const curr=String(ambulanceid);
+    const query=curr[curr.length-2]+curr[curr.length-1];
+    try {
+        const result=await ambulanceModel.findOne({
+            ambulanceid:query
+        });
+        if(result){
+            return res.json(result);
+        }else{
+            console.log('amb not found');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const nearestAmbulance = async (longitude,latitude) => {
+    // console.log('i am here\n');
+    // console.log(maxDistance);
+    try {
+        const newamb = await ambulanceModel.find({
+            location : {
+              $near : {
+                    $geometry : {
+                        type : "Point",
+                        coordinates : [longitude,latitude]
+                    }, 
+                    $maxDistance : 5000       
+                }
+            }
+        }).exec();
+        console.log(newamb.length);
+        return newamb;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports={findAmbulance,createAmbulance,nearestAmbulance}
--- routes/ambulance.js
+++ routes/ambulance.js
@@ -1,47 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const ambulanceModel = require("../model/ambulance");
-const isAuth = require('../user/authentication/isAuth')
+const isAuth = require('../user/authentication/isAuth');
+const { createAmbulance, nearestAmbulance, findAmbulance } = require("../controller/ambulanceController");
 
-router.post('/',)
+router.post('/',createAmbulance)
 
 //Fetching nearby Ambulancedata depending upon maxDistance specified from user
-const nearestAmbulance = async (longitude,latitude) => {
-    // console.log('i am here\n');
-    // console.log(maxDistance);
-    try {
-        const newamb = await ambulanceModel.find({
-            location : {
-              $near : {
-                    $geometry : {
-                        type : "Point",
-                        coordinates : [longitude,latitude]
-                    }, 
-                    $maxDistance : 5000       
-                }
-            }
-        }).exec();
-        console.log(newamb.length);
-        return newamb;
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 
-router.get('/info/:ambulanceid',  (req,res) => {
-    const {ambulanceid} = req.params;
-    // console.log(ambulanceid,"id from server");
-    ambulanceModel.findOne({
-        ambulanceid
-    }).then((result) => {
-        // console.log(`ambulance found with ${result.displayName}`);
-        // console.log(result.displayName);
-        return res.json(result);
-    }).catch((err) => {
-        console.log(err);
-    })
-})
+
+router.get('/info/:ambulanceid', findAmbulance )
 
 module.exports = {
     method : router,
